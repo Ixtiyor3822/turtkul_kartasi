28675c6d4db71cfa539b1bc7940173d2520cae27
Ixtiyor3822 <ruslanaymbetov@gmail.com>
2021-06-10 23:39:07 +0500

    Loyiha qismlarga bo`lindi

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="row m-1">
+      <div className="col">
+        Bu yerga Turtkul logotipi, va bir ikkita tanlovchilar bo`ladi.
+      </div>
+      <div className="row">
+        <div className="col-9">Bu yerga karta tushiriladi</div>
+        <div className="col-3">inputlar bu yerga tushiriladi.</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
